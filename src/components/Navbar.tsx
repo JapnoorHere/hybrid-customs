73edf7e8bc5c59eff7f85ff28ee305c72c60d395
Japnoor Singh <japnoor.singh@jungleworks.com>
2026-05-16 23:43:44 +0530

    Phone icon changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -149,14 +149,15 @@ export default function Navbar() {
                 <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
               </svg>
             </a>
-            <div className="h-4 w-[1px] bg-white/10" />
             {/* Phone */}
             <a
               href="tel:+919988229191"
-              className="flex flex-col text-right group"
+              aria-label="Call us"
+              className="text-white/70 hover:text-primary transition-colors duration-300"
             >
-              <span className="text-[9px] text-white/50 font-black tracking-[0.2em] leading-none mb-1 group-hover:text-white/70 transition-colors">CALL US</span>
-              <span className="text-[11px] text-primary font-black tracking-[0.05em] leading-none drop-shadow-[0_0_10px_rgba(253,186,18,0.3)] group-hover:text-white transition-colors">+91 99882 29191</span>
+              <svg viewBox="0 0 24 24" className="w-[16px] h-[16px] fill-none stroke-current" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
+                <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07A19.5 19.5 0 013.07 9.81a19.79 19.79 0 01-3.07-8.67A2 2 0 012 0h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L6.09 7.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 14.92z" />
+              </svg>
             </a>
             <div className="h-4 w-[1px] bg-white/10" />
             <div className="flex flex-col text-right">
